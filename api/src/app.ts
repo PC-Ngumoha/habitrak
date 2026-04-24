cccb40b83edfbd7ff617d2a2f9304c6c9ca6687e
PC-Ngumoha <pcngumoha@gmail.com>
2026-04-24 07:39:16 +0100

    Setting up user model, routes and controllers

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { errorHandler } from './middleware/errorHandler';
 import habitRoutes from './routes/habitRoutes';
+import userRoutes from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 
 // Routers
+app.use('/api/users', userRoutes);
 app.use('/api/habits', habitRoutes);
 
 // Global error handler
---
+++ api/src/controllers/userController.ts
@@ -0,0 +1,11 @@
+/**
+ * userController.ts
+ */
+
+import { NextFunction, Request, Response } from "express";
+
+export const register = async (req: Request, res: Response, next: NextFunction) => {}
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {}
+
+export const logout = async (req: Request, res: Response, next: NextFunction) => {}
