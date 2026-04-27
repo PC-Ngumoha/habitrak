315c5115c61d68446fc9d0fec26edf86d22ca262
PC-Ngumoha <pcngumoha@gmail.com>
2026-04-27 17:58:09 +0100

    Refactor userController to class UserController

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -8,106 +8,102 @@ import jwt from "jsonwebtoken";
 import * as bcrypt from 'bcryptjs';
 
 
-export const register = async (req: Request, res: Response, next: NextFunction) => {
+class UserController {
+  saltRounds: number;
 
-  try {
+  constructor() {
+    this.saltRounds = parseInt(process.env.SALT_ROUNDS as string, 10);
+  }
 
-    const { firstName, lastName, email, password } = req.body;
-
-    if (!(firstName && lastName && email && password)) {
-      res.status(400).send("You must provide all required fields")
-    }
-
-    const existingUser = await prisma.user.findFirst({ where: { email }});
-
-    if (existingUser) {
-      return res.status(409).send("User already exists. Login instead.");
-    }
-
-    const saltRounds = parseInt(process.env.SALT_ROUNDS as string, 10);
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    // Save user to DB
-    const user = await prisma.user.create({ data: {
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      token: '' // Yet to create access token
-    },
-    omit: {
-      password: true
-    }
-  });
-
-    // Create an access token & add to user.
-    const token = jwt.sign(
-      { user_id: user.id, email },
+  _generateToken(user: any): string {
+    return jwt.sign(
+      { user_id: user.id, email: user.email },
       process.env.JWT_SECRET as string,
       {
         expiresIn: "5h",
       }
-    )
-
-    user.token = token;
-
-    // return new user
-    res.status(201).json(user);
-
-  } catch (error) {
-    console.error(error);
-    next(error);
+    );
   }
-}
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
+  register = async (req: Request, res: Response, next: NextFunction) => {
 
-  try {
-    const {email, password} = req.body;
+    try {
 
-    if (!(email && password)) {
-      res.status(400).send("You must provide all required fields");
-    }
+      const { firstName, lastName, email, password } = req.body;
 
-    const user = await prisma.user.findFirst({ where: { email } });
+      if (!(firstName && lastName && email && password)) {
+        res.status(400).send("You must provide all required fields")
+      }
 
-    if ( user && (await bcrypt.compare(password, user.password))) {
-      const token = jwt.sign(
-        { user_id: user.id, email },
-        process.env.JWT_SECRET as string,
-        {
-          expiresIn: "5h",
+      const existingUser = await prisma.user.findFirst({ where: { email }});
+
+      if (existingUser) {
+        return res.status(409).send("User already exists. Login instead.");
+      }
+      const hashedPassword = await bcrypt.hash(password, this.saltRounds);
+
+      // Save user to DB
+      const user = await prisma.user.create({ data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+          token: '' // Yet to create access token
+        },
+        omit: {
+          password: true
         }
-      );
+      });
 
-      user.token = token;
+      user.token = this._generateToken(user);
+      // return new user
+      res.status(201).json(user);
+
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+
+  login = async (req: Request, res: Response, next: NextFunction) => {
+
+    try {
+      const {email, password} = req.body;
+
+      if (!(email && password)) {
+        res.status(400).send("You must provide all required fields");
+      }
+
+      const user = await prisma.user.findFirst({ where: { email } });
+
+      if ( user && (await bcrypt.compare(password, user.password))) {
+        user.token = this._generateToken(user);
+        // Remove password
+        const { password, ...safeUser} = user;
+
+        return res.json(safeUser);
+      }
+
+      res.status(401).send("Invalid user data provided.");
+
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  }
+
+   currentUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const user = await prisma.user.findFirst({ where: { id: req.user!.user_id }});
 
       // Remove password
-      const { password, ...safeUser} = user;
+      const { password, ...safeUser} = user!;
 
-      return res.json(safeUser);
+      res.json(safeUser);
+    } catch (error) {
+      next(error);
     }
-
-    res.status(401).send("Invalid user data provided.");
-
-  } catch (error) {
-    console.error(error);
-    next(error);
   }
 }
 
-export const currentUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const user = await prisma.user.findFirst({ where: { id: req.user!.user_id }});
-
-    // Remove password
-    const { password, ...safeUser} = user!;
-
-    res.json(safeUser);
-  } catch (error) {
-    next(error);
-  }
-}
-
-//FIXME: This is not looking necessary at the moment.
-// export const logout = async (req: Request, res: Response, next: NextFunction) => {}
+export default UserController;
--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -2,15 +2,16 @@
  * userRoutes.ts
  */
 import { Router } from "express";
-import {register, login, currentUser} from '../controllers/userController';
+import UserController from '../controllers/userController';
 import auth from '../middleware/auth';
 
 const router = Router();
+const controller = new UserController();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', controller.register);
+router.post('/login', controller.login);
 
-router.get('/me', auth, currentUser);
+router.get('/me', auth, controller.currentUser);
 // router.post('/logout', logout);
 
 export default router;
